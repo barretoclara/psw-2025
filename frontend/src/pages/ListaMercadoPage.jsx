a48928ad9bf72d73b606cf88069c94cdc6d65025
kaiosantos1 <165516524+kaiosantos1@users.noreply.github.com>
2025-06-29 00:08:45 -0300

    fix Lista de mercado visual

--- frontend/src/pages/ListaMercadoPage.jsx
+++ frontend/src/pages/ListaMercadoPage.jsx
@@ -2,7 +2,6 @@ import ListaMercado from '../components/listaMercado/ListaMercado';
 
 const ListaMercadoPage = () => (
   <div>
-    <h1 className="text-xl font-bold mb-4">Lista de Mercado</h1>
     <ListaMercado />
   </div>
 );
